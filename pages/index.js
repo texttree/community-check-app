868d8411f90435038e19fd65ed61a16ca493f10c
Vorobusec <timur.yancev08@gmail.com>
2023-09-16 07:16:04 +0400

    i18 in front page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Link from 'next/link'
 import { useUser } from '@supabase/auth-helpers-react'
+import { useTranslation } from 'next-i18next'
 
 const Index = () => {
+  const { t } = useTranslation()
   const user = useUser()
 
   if (user?.email) {
     return (
       <div className="bg-gray-200 min-h-screen">
         <div className="max-w-6xl mx-auto p-4 text-center">
-          <h1 className="text-3xl font-bold mb-4">Добро пожаловать в Community Check</h1>
-          <p className="text-lg mb-4">Вы уже вошли в систему.</p>
+          <h1 className="text-3xl font-bold mb-4">{t('welcomeTo')}</h1>
+          <p className="text-lg mb-4">{t('youLoggedIn')}</p>
           <Link
             href="/projects"
             className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md inline-flex items-center"
@@ -24,15 +26,13 @@ const Index = () => {
   return (
     <div className="bg-gray-200 min-h-screen">
       <div className="max-w-6xl mx-auto p-4 text-center">
-        <h1 className="text-3xl font-bold mb-4">Добро пожаловать в Community Check</h1>
-        <p className="text-lg mb-4">
-          Пожалуйста, войдите, чтобы получить доступ к полному функционалу.
-        </p>
+        <h1 className="text-3xl font-bold mb-4">{t('welcomeTo')}</h1>
+        <p className="text-lg mb-4">{t('loginToAccess')}</p>
         <Link
           href="/login"
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md inline-flex items-center"
         >
-          Войти
+          {t('comeON')}
         </Link>
       </div>
     </div>
