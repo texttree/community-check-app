174c3375230914ef21971096b2d8e09f3d93d03b
Vitek Foxprogs <foxprogs@gmail.com>
2024-02-20 10:48:32 +0400

    register page

--- pages/login.js
+++ pages/login.js
@@ -104,6 +104,12 @@ export default function Login() {
             >
               {t('signIn')}
             </button>
+            <Link
+              className="flex mt-6 w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              href={'/register'}
+            >
+              {t('register')}
+            </Link>
           </div>
         </div>
       )}
