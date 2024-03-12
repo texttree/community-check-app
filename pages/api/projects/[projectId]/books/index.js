3464b03a80a23b5009f6dfcf1280f8ea81f0dc9e
Denis Arger <den.arger@gmail.com>
2024-03-12 10:21:15 +0300

    fix: delete console.log

--- pages/api/projects/[projectId]/books/index.js
+++ pages/api/projects/[projectId]/books/index.js
@@ -49,7 +49,6 @@ export default async function handler(req, res) {
               book_name: name,
             }
           )
-          console.log(newBook)
           if (createError) throw createError
 
           return res.status(200).json(newBook)
