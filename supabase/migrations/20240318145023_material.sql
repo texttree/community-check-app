18066ee754afa564d2bea9b7d011a63cf131544c
Denis Arger <den.arger@gmail.com>
2024-03-18 17:54:05 +0300

    fix: for public.inspectors used  LEFT JOIN

--- supabase/migrations/20240318145023_material.sql
+++ supabase/migrations/20240318145023_material.sql
@@ -17,16 +17,15 @@ $function$
 DECLARE
     current_check record;
 BEGIN
-    SELECT c.id, i.id, m.id, c.finished_at
+    SELECT c.id, m.id, c.finished_at
     INTO current_check
     FROM public.checks AS c
-    INNER JOIN public.inspectors AS i ON i.check_id = c.id
     INNER JOIN public.materials AS m ON m.check_id = c.id
-    WHERE c.id = p_check_id 
+    LEFT JOIN public.inspectors AS i ON i.check_id = c.id
+    WHERE c.id = p_check_id
         AND c.deleted_at IS NULL
         AND c.finished_at > now()
-        AND (i.id = inspector_id OR inspector_id IS NULL) -- Учитываем возможность NULL
-        AND i.deleted_at IS NULL
+        AND (i.id = inspector_id OR inspector_id IS NULL)
         AND m.id = material_id
         AND m.deleted_at IS NULL;
 
@@ -41,8 +40,6 @@ BEGIN
     RETURN true;
 END;
 $function$;
-
-
 
 CREATE OR REPLACE FUNCTION get_notes_by_check_id(p_check_id uuid)
 RETURNS JSON
