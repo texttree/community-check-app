13089072f14524a2ff287ae17a878131a444ecfd
Vorobusec <timur.yancev08@gmail.com>
2024-03-28 18:03:59 +0400

    fix migration

--- supabase/migrations/20240326143114_delete_note.sql
+++ supabase/migrations/20240326143114_delete_note.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION delete_note(note_id INTEGER) RETURNS BOOLEAN AS 
+CREATE OR REPLACE FUNCTION delete_note(note_id INTEGER, inspector_id UUID) RETURNS BOOLEAN AS 
 $$
 DECLARE
     note_deleted BOOLEAN := FALSE;
 BEGIN
-    DELETE FROM public.notes WHERE id = note_id;
+     DELETE FROM public.notes AS n WHERE n.id = note_id AND n.inspector_id = delete_note.inspector_id;
     
     GET DIAGNOSTICS note_deleted = ROW_COUNT;
     
