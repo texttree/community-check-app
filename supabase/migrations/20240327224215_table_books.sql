6027b0387746c35e394ebc69912f4526d625bcfc
DenisArger <den.arger@gmail.com>
2024-03-28 21:25:11 +0300

    fix:

--- supabase/migrations/20240327224215_table_books.sql
+++ supabase/migrations/20240327224215_table_books.sql
@@ -25,3 +25,41 @@ BEGIN
         project_id = p_project_id;
 END;
 $$ LANGUAGE plpgsql;
+
+
+ALTER TABLE public.checks
+RENAME COLUMN started_at TO created_at;
+
+ALTER TABLE public.checks
+ADD COLUMN started_at timestamp with time zone null;
+
+
+
+DROP FUNCTION IF EXISTS  public.get_checks_for_book;
+
+CREATE OR REPLACE FUNCTION get_checks_for_book(book_id_param bigint)
+RETURNS TABLE (
+    check_id uuid,
+    check_name text,
+    check_material_link text,
+    check_created_time timestamp with time zone,
+    check_started_time timestamp with time zone,
+    check_finished_time timestamp with time zone
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        id AS check_id,
+        name AS check_name,
+        material_link AS check_material_link,
+        created_at AS check_created_time,
+        started_at AS check_started_time,
+        finished_at AS check_finished_time
+    FROM
+        public.checks
+    WHERE
+        book_id = book_id_param
+        AND deleted_at IS NULL;
+END;
+$$ LANGUAGE plpgsql;
