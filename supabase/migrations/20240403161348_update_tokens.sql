c39a02d87a4712941e9a987db7c797fc62894c7f
DenisArger <den.arger@gmail.com>
2024-04-03 17:35:14 +0300

    feat: update add_token and add delete_token

--- supabase/migrations/20240403161348_update_tokens.sql
+++ supabase/migrations/20240403161348_update_tokens.sql
@@ -1,20 +1,23 @@
 ALTER TABLE public.tokens
 ADD COLUMN name text;
 
-DROP FUNCTION IF EXISTS public.add_token();
+DROP FUNCTION IF EXISTS public.add_token(text);
 
-CREATE OR REPLACE FUNCTION public.add_token(name text)
+CREATE OR REPLACE FUNCTION public.add_token(p_name text)
 RETURNS uuid AS $$
 DECLARE
     new_id uuid;
 BEGIN
+    IF EXISTS (SELECT 1 FROM public.tokens WHERE user_id = auth.uid() AND name = p_name) THEN
+        RAISE EXCEPTION 'Token with name % already exists for the current user', p_name;
+    END IF;
+
     INSERT INTO public.tokens (user_id, name)
-    VALUES (auth.uid(), name)
+    VALUES (auth.uid(), p_name)
     RETURNING id INTO new_id;
     
     RETURN new_id;
 END;
-
 $$ LANGUAGE plpgsql;
 
 
@@ -23,5 +26,14 @@ RETURNS SETOF public.tokens AS $$
 BEGIN
     RETURN QUERY
         SELECT * FROM public.tokens WHERE user_id = auth.uid();
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION public.delete_token(token_id uuid)
+RETURNS void AS $$
+BEGIN
+    DELETE FROM public.tokens WHERE id = token_id;
 END;
 $$ LANGUAGE plpgsql;
