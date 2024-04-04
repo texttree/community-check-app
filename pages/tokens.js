7de55ba8f03621ccf1d32fe8909d03933a6f417d
DenisArger <den.arger@gmail.com>
2024-04-04 10:47:16 +0300

    feat: add check is empty token Name, delete error text

--- pages/tokens.js
+++ pages/tokens.js
@@ -3,20 +3,25 @@ import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 
+import toast, { Toaster } from 'react-hot-toast'
+
 const TokenGeneration = () => {
   const supabase = useSupabaseClient()
   const { t } = useTranslation()
-  const [accessToken, setAccessToken] = useState('')
-  const [tokenName, setTokenName] = useState('')
-  const [isTokenGenerated, setIsTokenGenerated] = useState(false)
-  const [errorText, setErrorText] = useState('')
+
+  const [token, setToken] = useState('')
   const [tokens, setTokens] = useState([])
+  const [tokenName, setTokenName] = useState('')
 
   useEffect(() => {
     fetchTokens()
   }, [])
 
   const handleGenerateToken = async () => {
+    if (tokenName.trim() === '') {
+      toast.error('Token name cannot be empty!')
+      return
+    }
     try {
       const { data, error } = await supabase.rpc('add_token', { p_name: tokenName })
 
@@ -24,14 +29,13 @@ const TokenGeneration = () => {
         throw new Error(`Failed to store token in the database: ${error.message}`)
       }
 
-      setAccessToken(data)
-      setIsTokenGenerated(true)
-      setErrorText('')
+      setToken(data)
       fetchTokens()
+
+      toast.success('Token generated successfully!')
     } catch (error) {
       console.error('Error generating or storing tokens:', error.message)
-      setIsTokenGenerated(false)
-      setErrorText('Failed to generate or store tokens')
+      toast.error('Failed to generate or store tokens')
     }
   }
 
@@ -50,7 +54,7 @@ const TokenGeneration = () => {
   }
 
   const handleCopyToken = () => {
-    navigator.clipboard.writeText(accessToken)
+    navigator.clipboard.writeText(token)
   }
 
   const handleDeleteToken = async (token_id) => {
@@ -62,6 +66,7 @@ const TokenGeneration = () => {
       }
 
       fetchTokens()
+      toast.success('Token deleted successfully!')
     } catch (error) {
       console.error('Error deleting token:', error.message)
     }
@@ -69,10 +74,11 @@ const TokenGeneration = () => {
 
   return (
     <div className="flex justify-center items-center flex-col mt-4">
+      <Toaster />
       <div className="flex items-center">
         <input
           type="text"
-          placeholder={t('tokenNamePlaceholder')}
+          placeholder={t('tokenName')}
           className="border border-gray-300 px-4 py-2 rounded-md mr-2"
           value={tokenName}
           onChange={(e) => setTokenName(e.target.value)}
@@ -84,15 +90,12 @@ const TokenGeneration = () => {
           {t('generateToken')}
         </button>
       </div>
-      {isTokenGenerated && (
-        <p className="text-green-500 mt-2">{t('tokenGeneratedSuccessfully')}</p>
-      )}
-      {errorText && <p className="text-red-500 mt-2">{errorText}</p>}
-      {accessToken && (
+
+      {token && (
         <div className="flex mt-4 items-center">
           <input
             type="text"
-            value={accessToken}
+            value={token}
             className="border border-gray-300 px-8 py-2 rounded-md mr-2 w-96"
             readOnly
           />
