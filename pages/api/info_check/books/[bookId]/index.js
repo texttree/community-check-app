e3241e8bd8ed2e940309650fc2fcfe7d929fa5f3
DenisArger <den.arger@gmail.com>
2024-04-05 18:16:32 +0300

    feat: add api for counterNotes

---
+++ pages/api/info_check/books/[bookId]/index.js
@@ -0,0 +1,29 @@
+import { supabaseService } from '@/helpers/supabaseService'
+
+export default async function handler(req, res) {
+  const {
+    query: { bookId },
+    method,
+  } = req
+  console.log(bookId, 8)
+  try {
+    switch (method) {
+      case 'GET':
+        const { data, error } = await supabaseService.rpc('get_notes_count_for_book', {
+          book_id: bookId,
+        })
+
+        if (error) {
+          throw error
+        }
+
+        return res.status(200).json(data)
+
+      default:
+        res.setHeader('Allow', ['GET'])
+        return res.status(405).end(`Method ${method} Not Allowed`)
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
