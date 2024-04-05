fd8e21da1c83b71e0369e5e138e5c1fa5d8438ba
DenisArger <den.arger@gmail.com>
2024-04-05 18:15:49 +0300

    feat: add sql function get_notes_count_for_book

--- supabase/migrations/20240305170739_info_checks.sql
+++ supabase/migrations/20240305170739_info_checks.sql
@@ -10,3 +10,30 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION get_notes_count_for_book(book_id bigint)
+RETURNS json AS
+$$
+DECLARE
+    result json;
+BEGIN
+    SELECT json_agg(json_build_object(
+        'check_id', subquery.id,
+        'notes_count', subquery.notes_count
+    ))
+    INTO result
+    FROM (
+        SELECT c.id, COUNT(n.id) AS notes_count
+        FROM checks c
+        LEFT JOIN materials m ON c.id = m.check_id
+        LEFT JOIN notes n ON m.id = n.material_id
+        WHERE c.book_id = get_notes_count_for_book.book_id
+        GROUP BY c.id
+    ) AS subquery;
+
+    RETURN result;
+END;
+$$
+LANGUAGE plpgsql;
