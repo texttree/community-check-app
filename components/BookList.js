b3d01272b0a388f491ed7156c7c695df5592db75
DenisArger <den.arger@gmail.com>
2024-04-10 15:19:28 +0300

    fix: ErrorMessage add in  BookList

--- components/BookList.js
+++ components/BookList.js
@@ -4,41 +4,6 @@ import useSWR from 'swr'
 import { fetcher } from '@/helpers/fetcher'
 import { formatDate } from '@/helpers/formatDate'
 
-const ErrorMessage = ({ message }) => <span className="text-red-600">{message}</span>
-
-const LoadingMessage = () => <span class="sr-only">{t('loading')}...</span>
-
-const BookChecksInfo = ({ projectId, bookId, showLastCheck }) => {
-  const { data: checks, error } = useSWR(
-    projectId && bookId && `/api/projects/${projectId}/books/${bookId}/checks`,
-    fetcher
-  )
-
-  const getLatestCheckDate = (checks) => {
-    if (checks && checks.length > 0) {
-      const validDates = checks
-        .filter((check) => check.check_started_time !== null)
-        .map((check) => new Date(check.check_started_time))
-
-      if (validDates.length > 0) {
-        const latestDate = new Date(Math.max(...validDates))
-        return formatDate(latestDate)
-      } else {
-        return '-'
-      }
-    }
-    return '-'
-  }
-
-  if (error) {
-    return <ErrorMessage message="Error" />
-  } else if (!checks) {
-    return <LoadingMessage />
-  } else {
-    return <span>{showLastCheck ? getLatestCheckDate(checks) : checks.length}</span>
-  }
-}
-
 const BookList = ({ projectId }) => {
   const { t } = useTranslation()
 
@@ -46,6 +11,41 @@ const BookList = ({ projectId }) => {
     projectId && `/api/projects/${projectId}/books`,
     fetcher
   )
+
+  const ErrorMessage = ({ message }) => <span className="text-red-600">{message}</span>
+
+  const LoadingMessage = () => <span class="sr-only">{t('loading')}...</span>
+
+  const BookChecksInfo = ({ projectId, bookId, showLastCheck }) => {
+    const { data: checks, error } = useSWR(
+      projectId && bookId && `/api/projects/${projectId}/books/${bookId}/checks`,
+      fetcher
+    )
+
+    const getLatestCheckDate = (checks) => {
+      if (checks && checks.length > 0) {
+        const validDates = checks
+          .filter((check) => check.check_started_time !== null)
+          .map((check) => new Date(check.check_started_time))
+
+        if (validDates.length > 0) {
+          const latestDate = new Date(Math.max(...validDates))
+          return formatDate(latestDate)
+        } else {
+          return '-'
+        }
+      }
+      return '-'
+    }
+
+    if (error) {
+      return <ErrorMessage message="Error" />
+    } else if (!checks) {
+      return <LoadingMessage />
+    } else {
+      return <span>{showLastCheck ? getLatestCheckDate(checks) : checks.length}</span>
+    }
+  }
 
   return (
     <>
