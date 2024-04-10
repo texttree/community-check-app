d02a77822057f55854606ff02b47ba95a6f54062
DenisArger <den.arger@gmail.com>
2024-04-10 16:54:27 +0300

    fix: update get_tokens and  delete_token

--- supabase/migrations/20240410233012_update_tokens.sql
+++ supabase/migrations/20240410233012_update_tokens.sql
@@ -1,35 +1,21 @@
-DROP TABLE IF EXISTS public.tokens CASCADE;
+DROP FUNCTION IF EXISTS public.get_tokens;
 
-CREATE TABLE public.tokens (
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
-    token UUID NOT NULL DEFAULT uuid_generate_v4(),
-    user_id UUID NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NULL DEFAULT NOW(),
-    name TEXT NULL,
-    CONSTRAINT tokens_pkey PRIMARY KEY (id),
-    CONSTRAINT tokens_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
-) TABLESPACE pg_default;
-
-
-DROP FUNCTION IF EXISTS public.add_token;
-
-CREATE OR REPLACE FUNCTION public.add_token(p_name text)
-RETURNS uuid AS $$
-DECLARE
-    new_token uuid;
+CREATE OR REPLACE FUNCTION get_tokens()
+RETURNS TABLE (p_name text, p_created_at timestamp with time zone)
+AS $$
 BEGIN
-    IF TRIM(p_name) = '' THEN
-        RAISE EXCEPTION 'Token name cannot be empty';
-    END IF;
+    RETURN QUERY
+    SELECT name, created_at
+    FROM public.tokens
+    WHERE user_id = auth.uid();
+END;
+$$ LANGUAGE plpgsql;
 
-    IF EXISTS (SELECT 1 FROM public.tokens WHERE user_id = auth.uid() AND LOWER(name) = LOWER(TRIM(p_name))) THEN
-        RAISE EXCEPTION 'Token with name % already exists for the current user', TRIM(p_name);
-    END IF;
+DROP FUNCTION IF EXISTS public.delete_token;
 
-    INSERT INTO public.tokens (user_id, name)
-    VALUES (auth.uid(), LOWER(TRIM(p_name)))
-    RETURNING token INTO new_token;
-    
-    RETURN new_token;
+CREATE OR REPLACE FUNCTION public.delete_token(token_name text)
+RETURNS void AS $$
+BEGIN
+    DELETE FROM public.tokens WHERE name = token_name;
 END;
 $$ LANGUAGE plpgsql;
