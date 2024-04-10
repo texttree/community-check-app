51f166fb1080c839e659907e953cc279c742bcd0
DenisArger <den.arger@gmail.com>
2024-04-10 14:57:17 +0300

    feat: add style fo loading data

--- pages/checks/[checkId]/[inspectorId]/chapter/[chapterNumber]/index.js
+++ pages/checks/[checkId]/[inspectorId]/chapter/[chapterNumber]/index.js
@@ -104,7 +104,17 @@ const CheckDetail = () => {
     <div className="bg-gray-200">
       {isLoading && (
         <div className="max-w-6xl mx-auto p-4 text-center">
-          <p className="text-2xl text-blue-500">{t('loading')}</p>
+          <p>
+            <div role="status" class="max-w-sm animate-pulse">
+              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+              <span class="sr-only">{t('loading')}...</span>
+            </div>
+          </p>
         </div>
       )}
       {!isLoading && !material && (
