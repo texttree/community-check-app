f95844638ba99c09616f9ba09cde094c362af111
DenisArger <den.arger@gmail.com>
2024-04-15 15:54:55 +0300

    feat: add check auth

--- pages/api/info_check/[checkId]/index.js
+++ pages/api/info_check/[checkId]/index.js
@@ -1,18 +1,34 @@
 import { supabaseService } from '@/helpers/supabaseService'
+import serverApi from '@/helpers/serverApi'
 
 export default async function handler(req, res) {
+  let userId
+
   const {
     query: { checkId },
     method,
   } = req
 
   try {
+    const _supabase = await serverApi(req, res)
+    const {
+      data: {
+        user: { id },
+      },
+    } = await _supabase.auth.getUser()
+    userId = id
+    console.log(id, 16)
+  } catch (Error) {
+    userId = null
+  }
+
+  try {
     switch (method) {
       case 'GET':
-        const { data, error } = await supabaseService.rpc('get_check_and_book_names', {
-          checks_id: checkId,
+        const { data, error } = await supabaseService.rpc('get_check_info', {
+          check_id: checkId,
+          user_id: userId,
         })
-
         if (error) {
           throw error
         }
