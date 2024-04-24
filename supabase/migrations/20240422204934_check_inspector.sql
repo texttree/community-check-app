89d15922cf3f8c28830b9be09baa5395e96d4034
DenisArger <den.arger@gmail.com>
2024-04-24 12:42:37 +0300

    Update 20240422204934_check_inspector.sql

--- supabase/migrations/20240422204934_check_inspector.sql
+++ supabase/migrations/20240422204934_check_inspector.sql
@@ -27,22 +27,50 @@ CREATE OR REPLACE FUNCTION has_notes(p_inspector_id uuid)
 $$
 LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS public.delete_inspector_and_notes(uuid);
+DROP FUNCTION IF EXISTS public.delete_inspector_and_notes;
 
-CREATE OR REPLACE FUNCTION delete_inspector_and_notes(p_inspector_id uuid, p_delete_notes boolean)
+CREATE OR REPLACE FUNCTION delete_inspector_and_notes(p_user_id uuid, p_inspector_id uuid, p_delete_notes boolean)
 RETURNS void AS
 $$
 BEGIN
-    IF p_delete_notes THEN
-        UPDATE public.notes
-        SET deleted_at = now()
-        WHERE inspector_id = p_inspector_id;
-    END IF;
+    IF check_inspector_user_relation(p_user_id, p_inspector_id) THEN
+        IF p_delete_notes THEN
+            UPDATE public.notes
+            SET deleted_at = now()
+            WHERE inspector_id = p_inspector_id;
+        END IF;
     
-    UPDATE public.inspectors
-    SET deleted_at = now()
-    WHERE id = p_inspector_id;
+        UPDATE public.inspectors
+        SET deleted_at = now()
+        WHERE id = p_inspector_id;
+    ELSE
+        RAISE EXCEPTION 'Inspector is not related to the user';
+    END IF;
 END;
 $$
 LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION check_inspector_user_relation(p_user_id uuid, p_inspector_id uuid)
+RETURNS BOOLEAN AS $$
+DECLARE
+    v_project_id bigint;
+BEGIN
+    SELECT id INTO v_project_id
+    FROM public.projects
+    WHERE user_id = p_user_id;
+
+    RETURN EXISTS (
+        SELECT 1
+        FROM public.projects pr
+        JOIN public.books b ON pr.id = b.project_id
+        JOIN public.checks c ON b.id = c.book_id
+        JOIN public.inspectors i ON c.id = i.check_id
+        WHERE pr.id = v_project_id
+        AND i.id = p_inspector_id
+     
+    );
+END;
+$$ LANGUAGE plpgsql;
+
 
