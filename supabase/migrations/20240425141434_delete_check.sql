49763678e8e5c2e970a663e5fa505aa86936daf5
DenisArger <den.arger@gmail.com>
2024-04-25 22:13:12 +0300

    feat: add soft_delete_book and replace get_books_by_project

--- supabase/migrations/20240425141434_delete_check.sql
+++ supabase/migrations/20240425141434_delete_check.sql
@@ -36,3 +36,41 @@ BEGIN
 
 END;
 $$;
+
+
+CREATE OR REPLACE FUNCTION soft_delete_book(p_book_id bigint, p_user_id uuid)
+RETURNS void AS
+$$
+DECLARE
+    check_id uuid;
+BEGIN
+    UPDATE public.books
+    SET deleted_at = NOW()
+    WHERE id = p_book_id;
+
+    FOR check_id IN SELECT id FROM public.checks WHERE book_id = p_book_id LOOP
+        PERFORM soft_delete_check(check_id, p_user_id);
+    END LOOP;
+
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION "public"."get_books_by_project"("p_project_id" bigint) 
+RETURNS TABLE("book_id" bigint, "book_name" "text", "book_created_at" timestamp with time zone, "book_deleted_at" timestamp with time zone)
+LANGUAGE "plpgsql"
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        id as book_id,
+        name as book_name,
+        created_at as book_created_at, 
+        deleted_at as book_deleted_at
+    FROM
+        public.books
+    WHERE
+        project_id = p_project_id
+        AND deleted_at IS NULL;
+END;
+$$;
