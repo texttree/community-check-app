21608d0d8d5648797e08608ae7cb24f588dc05d6
DenisArger <den.arger@gmail.com>
2024-05-03 12:31:46 +0300

    feat: add api dor delete book

--- app/api/projects/[projectId]/books/[bookId]/route.js
+++ app/api/projects/[projectId]/books/[bookId]/route.js
@@ -52,3 +52,33 @@ export async function POST(req, { params: { bookId } }) {
     return Response.json({ error }, { status: 500 })
   }
 }
+
+export async function DELETE(req, { params: { bookId } }) {
+  const headersList = headers()
+  const userId = headersList.get('x-user-id')
+  if (!userId) {
+    return Response.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+
+  const supabase = createClient()
+  try {
+    const { error } = await supabase.rpc('delete_book', {
+      p_user_id: userId,
+      p_book_id: bookId,
+    })
+
+    if (error) {
+      throw error
+    }
+
+    return Response.json({ message: 'Book deleted successfully' }, { status: 200 })
+  } catch (error) {
+    if (error.message.includes('Permission denied')) {
+      return Response.json(
+        { error: 'You do not have permission to delete this book' },
+        { status: 403 }
+      )
+    }
+    return Response.json({ error: 'An error occurred during deletion' }, { status: 500 })
+  }
+}
