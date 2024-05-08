0c57599c64be9a3a3844ad9e609b45504113297f
Vitek Foxprogs <foxprogs@gmail.com>
2024-05-09 01:47:34 +0400

    db fixed

--- supabase/migrations/20240507145523_rename_argument.sql
+++ supabase/migrations/20240507145523_rename_argument.sql
@@ -1,4 +1,7 @@
-DROP FUNCTION IF EXISTS soft_delete_check;
+DROP FUNCTION IF EXISTS soft_delete_check(p_check_id uuid, p_user_id uuid);
+DROP FUNCTION IF EXISTS soft_delete_check(check_id uuid, user_id uuid);
+
+DROP FUNCTION IF EXISTS delete_project;
 
 CREATE OR REPLACE FUNCTION soft_delete_check(check_id uuid, user_id uuid)
     RETURNS void
@@ -9,10 +12,10 @@ DECLARE
 BEGIN
     SELECT (p.user_id = soft_delete_check.user_id)
     INTO check_owner
-    FROM public.checks c
-    INNER JOIN public.books b ON c.book_id = b.id
-    INNER JOIN public.projects p ON b.project_id = p.id
-    WHERE c.id = soft_delete_check.check_id;
+    FROM public.checks as c
+    INNER JOIN public.books as b ON c.book_id = b.id
+    INNER JOIN public.projects as p ON b.project_id = p.id
+    WHERE c.id = soft_delete_check.check_id and c.deleted_at is NULL;
 
     IF NOT check_owner THEN
         RAISE EXCEPTION 'Permission denied. You are not the owner of this check.';
@@ -22,13 +25,9 @@ BEGIN
     SET deleted_at = NOW()
     WHERE id = soft_delete_check.check_id;
 
-    UPDATE public.materials
-    SET deleted_at = NOW()
-    WHERE materials.check_id = soft_delete_check.check_id;
-
     UPDATE public.notes
     SET deleted_at = NOW()
-    WHERE material_id IN (SELECT id FROM public.materials WHERE materials.check_id = soft_delete_check.check_id);
+    WHERE notes.check_id = soft_delete_check.check_id;
 
     UPDATE public.inspectors
     SET deleted_at = NOW()
@@ -37,22 +36,24 @@ BEGIN
 END;
 $$;
 
-DROP FUNCTION IF EXISTS delete_book;
+DROP FUNCTION IF EXISTS delete_book(user_id uuid, book_id bigint);
+
+DROP FUNCTION IF EXISTS delete_book(p_user_id uuid, p_book_id bigint);
 
 CREATE OR REPLACE FUNCTION delete_book(user_id uuid, book_id bigint) RETURNS void AS $$
 DECLARE
     user_exists boolean;
 BEGIN
     SELECT EXISTS (
-        SELECT 1 
+        SELECT 1
         FROM public.books b
         JOIN public.projects p ON b.project_id = p.id
-        WHERE b.id = delete_book.book_id AND p.user_id = delete_book.user_id 
+        WHERE b.id = delete_book.book_id AND p.user_id = delete_book.user_id
     ) INTO user_exists;
 
     IF user_exists THEN
         DELETE FROM public.books
-        WHERE id = delete_book.book_id;
+        WHERE books.id = delete_book.book_id;
     ELSE
         RAISE EXCEPTION 'Permission denied. You are not the owner of this book.';
     END IF;
@@ -60,4 +61,68 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-DROP FUNCTION IF EXISTS delete_project;
+ALTER TABLE ONLY "public"."books"
+    DROP CONSTRAINT IF EXISTS "books_project_id_fkey";
+
+ALTER TABLE ONLY "public"."books"
+    ADD CONSTRAINT "books_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE CASCADE;
+
+
+ALTER TABLE ONLY "public"."checks"
+    DROP CONSTRAINT IF EXISTS "checks_book_id_fkey";
+
+ALTER TABLE ONLY "public"."checks"
+    ADD CONSTRAINT "checks_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "public"."books"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."inspectors"
+    DROP CONSTRAINT IF EXISTS "inspectors_check_id_fkey";
+
+ALTER TABLE ONLY "public"."inspectors"
+    ADD CONSTRAINT "inspectors_check_id_fkey" FOREIGN KEY ("check_id") REFERENCES "public"."checks"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."notes"
+    DROP CONSTRAINT IF EXISTS "notes_inspector_id_fkey";
+
+ALTER TABLE ONLY "public"."notes"
+    ADD CONSTRAINT "notes_inspector_id_fkey" FOREIGN KEY ("inspector_id") REFERENCES "public"."inspectors"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."notes"
+    DROP CONSTRAINT IF EXISTS "notes_material_id_fkey";
+
+ALTER TABLE ONLY "public"."notes"
+    DROP CONSTRAINT IF EXISTS "notes_check_id_fkey";
+
+ALTER TABLE ONLY "public"."notes"
+    ADD CONSTRAINT "notes_check_id_fkey" FOREIGN KEY ("check_id") REFERENCES "public"."checks"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."projects"
+    DROP CONSTRAINT IF EXISTS "projects_user_id_fkey";
+
+ALTER TABLE ONLY "public"."projects"
+    ADD CONSTRAINT "projects_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."tokens"
+    DROP CONSTRAINT IF EXISTS "tokens_user_id_fkey";
+
+ALTER TABLE ONLY "public"."tokens"
+    ADD CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."users"
+    DROP CONSTRAINT IF EXISTS "users_id_fkey";
+
+ALTER TABLE ONLY "public"."users"
+    ADD CONSTRAINT "users_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
+
+CREATE OR REPLACE FUNCTION delete_project(
+    p_project_id bigint,
+    p_user_id uuid
+)
+RETURNS void AS
+$$
+BEGIN
+    DELETE FROM public.projects
+    WHERE id = p_project_id
+    AND user_id = p_user_id;
+END;
+$$
+LANGUAGE plpgsql;
