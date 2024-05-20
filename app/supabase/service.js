e06b360641d331c201e360ddededb7a5667e2f79
DenisArger <den.arger@gmail.com>
2024-05-20 11:45:20 +0300

    fix: add check .env

--- app/supabase/service.js
+++ app/supabase/service.js
@@ -4,6 +4,11 @@ const createSupabaseClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
   const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
 
+  if (!supabaseUrl || !supabaseServiceKey) {
+    console.error('Missing Supabase environment variables')
+    throw new Error('Missing Supabase environment variables')
+  }
+
   return createServiceClient(supabaseUrl, supabaseServiceKey, {
     auth: {
       persistSession: false,
