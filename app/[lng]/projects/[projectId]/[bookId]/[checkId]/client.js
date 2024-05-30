d8c3313ea94d4023c481f2335e4038db6b8c9f29
DenisArger <den.arger@gmail.com>
2024-05-30 14:52:58 +0300

    fix: add margin botton for button

--- app/[lng]/projects/[projectId]/[bookId]/[checkId]/client.js
+++ app/[lng]/projects/[projectId]/[bookId]/[checkId]/client.js
@@ -234,7 +234,7 @@ const CheckId = ({ lng }) => {
           value={materialLink}
           onChange={(e) => setMaterialLink(e.target.value)}
           placeholder={t('linkResource')}
-          className="mt-1 px-2 py-1 block rounded-lg border border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 w-full"
+          className="mt-1 mb-2 px-2 py-1 block rounded-lg border border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 w-full"
         />
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md inline-block"
