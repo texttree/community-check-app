de0dd394f52c68d1561dd6c2040d713021febbde
DenisArger <den.arger@gmail.com>
2024-06-04 21:49:55 +0300

    feat: used customs color for login page and fisrt page

--- app/[lng]/client.js
+++ app/[lng]/client.js
@@ -14,7 +14,7 @@ const Index = ({ lng, user }) => {
             <p className="text-lg mb-4">{t('youLoggedIn')}</p>
             <Link
               href={`/${lng}/projects`}
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md inline-flex items-center"
+              className="bg-desaturated-cyan hover:bg-deep-space text-white px-4 py-2 rounded-md inline-flex items-center"
             >
               {t('goToProjects')}
             </Link>
@@ -24,7 +24,7 @@ const Index = ({ lng, user }) => {
             <p className="text-lg mb-4">{t('loginToAccess')}</p>
             <Link
               href="/login"
-              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md inline-flex items-center"
+              className="bg-desaturated-cyan hover:bg-deep-space text-white px-4 py-2 rounded-md inline-flex items-center"
             >
               {t('signIn')}
             </Link>
--- app/[lng]/login/client.js
+++ app/[lng]/login/client.js
@@ -88,13 +88,13 @@ export default function FormComponent({ lng, redirectedFrom }) {
 
         <p>{error}</p>
         <button
-          className="mt-6 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="mt-6 flex w-full justify-center rounded-md bg-desaturated-cyan px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-deep-space focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:deep-space"
           onClick={handleLogin}
         >
           {t('signIn')}
         </button>
         <button
-          className="flex mt-6 w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          className="flex mt-6 w-full justify-center rounded-md bg-desaturated-cyan px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-deep-space focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:deep-space"
           onClick={handleRegister}
         >
           {t('register')}
