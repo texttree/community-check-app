afd15b194f8f697816cd86e8bcbe00c8bd3b8d29
DenisArger <den.arger@gmail.com>
2024-06-07 17:37:55 +0300

    feat: add picture Paul

--- app/[lng]/login/client.js
+++ app/[lng]/login/client.js
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-
+import Image from 'next/image'
 import { useTranslation } from '@/app/i18n/client'
 import { createClient } from '@/app/supabase/client'
 import axios from 'axios'
@@ -12,9 +12,13 @@ export default function FormComponent({ lng, redirectedFrom }) {
   const router = useRouter()
   const { t } = useTranslation(lng, 'common')
   const [email, setEmail] = useState('')
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(null)
   const [password, setPassword] = useState('')
-  const handleLogin = async () => {
+  const [loading, setLoading] = useState(false)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    setLoading(true)
     try {
       const { error } = await supabaseClient.auth.signInWithPassword({
         email,
@@ -25,80 +29,91 @@ export default function FormComponent({ lng, redirectedFrom }) {
         throw error
       }
 
-      setError(false)
+      setError(null)
       router.push(redirectedFrom ?? '/' + lng + '/projects')
     } catch (error) {
       setError(error.message)
+    } finally {
+      setLoading(false)
     }
   }
-  const handleRegister = async () => {
+
+  const handleRegister = async (event) => {
+    event.preventDefault()
+    setLoading(true)
     try {
-      const {
-        data: { error },
-      } = await axios.post('/api/register', { email, password })
+      const response = await axios.post('/api/register', { email, password })
+      const { error } = response.data
+
       if (error) {
         throw error
       }
-      setError(false)
+
+      setError(null)
       router.push('/' + lng + '/projects')
     } catch (error) {
       console.error({ error })
       setError(error?.response?.data?.error?.message ?? 'Error')
+    } finally {
+      setLoading(false)
     }
   }
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 mb-4 tracking-tight text-gray-900">
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div className="relative flex flex-col items-center">
+        <Image src="/Paul.svg" alt="login illustration" width={500} height={500} />
+        <div className="absolute top-1/2 transform -translate-y-1/2 w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
+          <h2 className="text-center text-3xl font-bold text-gray-900 mb-6">
             {t('signAccount')}
           </h2>
-        </div>
-        <form className="space-y-6">
-          <div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">
-              {t('email')}
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              value={email}
-              onChange={({ target: { value } }) => setEmail(value)}
-              className="block w-full rounded-md px-2 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-          <div>
-            <div className="flex items-center justify-between">
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                {t('password')}
+          <form className="space-y-6" onSubmit={handleLogin}>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                {t('email')}
               </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={({ target: { value } }) => setEmail(value)}
+                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
             </div>
-            <div className="mt-2">
-              <input
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={({ target: { value } }) => setPassword(value)}
-                className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+            <div>
+              <label className="block text-sm font-medium text-gray-700">Пароль</label>
+              <div className="mt-1">
+                <input
+                  type="password"
+                  placeholder="password"
+                  value={password}
+                  onChange={({ target: { value } }) => setPassword(value)}
+                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
             </div>
-          </div>
-        </form>
-
-        <p>{error}</p>
-        <button
-          className="mt-6 flex w-full justify-center rounded-md bg-ming-blue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-deep-space focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:deep-space"
-          onClick={handleLogin}
-        >
-          {t('signIn')}
-        </button>
-        <button
-          className="flex mt-6 w-full justify-center rounded-md bg-ming-blue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-deep-space focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:deep-space"
-          onClick={handleRegister}
-        >
-          {t('register')}
-        </button>
+            <div className="mt-6 flex justify-between">
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-2"
+                disabled={loading}
+              >
+                {loading ? 'Загрузка...' : 'Войти'}
+              </button>
+              <button
+                type="button"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                onClick={handleRegister}
+                disabled={loading}
+              >
+                {loading ? 'Загрузка...' : 'Регистрация'}
+              </button>
+            </div>
+          </form>
+          {error && <p className="mt-2 text-center text-sm text-red-600">{error}</p>}
+        </div>
       </div>
     </div>
   )
