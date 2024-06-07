2a95161c3bdf1b586368db7941fa563f1ade57a5
DenisArger <den.arger@gmail.com>
2024-06-07 22:15:37 +0300

    feat: add migration for fix bags auth

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -36,3 +36,20 @@ INSERT INTO "public"."inspectors" ("id","name","check_id") VALUES
 ('11111111-1111-1111-1111-111111111112','Abraham','11111111-1111-1111-1111-111111111112'),
 ('11111111-1111-1111-1111-111111111113','Abraham','11111111-1111-1111-1111-111111111113'),
 ('11111111-1111-1111-1111-111111111114','Abraham','11111111-1111-1111-1111-111111111114');
+
+
+--Добавляем миграции, связанные с ошибкой авторизации
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM auth.schema_migrations WHERE version = '20221125140132') THEN
+        INSERT INTO auth.schema_migrations(version) VALUES ('20221125140132');
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM auth.schema_migrations WHERE version = '20221208132122') THEN
+        INSERT INTO auth.schema_migrations(version) VALUES ('20221208132122');
+    END IF;
+END $$;
+
