1f28f1c702f7916a2fc14b41aeed839e6150a34c
DenisArger <den.arger@gmail.com>
2024-06-24 15:46:26 +0300

    feat: add Menu for page Projуct

--- app/[lng]/projects/client.js
+++ app/[lng]/projects/client.js
@@ -1,19 +1,29 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Link from 'next/link'
 import { useTranslation } from '@/app/i18n/client'
 import axios from 'axios'
-
 import Projects from '@/app/components/Projects'
 import AddProjectModal from '@/app/components/AddProjectModal'
 import { useRouter } from 'next/navigation'
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import {
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@headlessui/react'
+import { Menu } from '@headlessui/react'
 
 const ProjectPage = ({ lng }) => {
   const { t } = useTranslation(lng, 'common')
-
   const [showAddModal, setShowAddModal] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuRef = useRef(null)
   const router = useRouter()
 
   const openAddModal = () => {
@@ -44,55 +54,117 @@ const ProjectPage = ({ lng }) => {
     }
   }
 
-  return (
-    <div>
-      <div className="max-w-6xl mx-auto p-4">
-        <TabGroup>
-          <TabList className="bg-ming-blue flex p-2 w-full border border-th-secondary-300 rounded-xl shadow-md">
-            <Tab
-              className={({ selected }) =>
-                selected
-                  ? 'bg-ming-blue text-white cursor-pointer text-2xl font-bold  px-9 py-2 rounded-md'
-                  : 'text-blue-100 hover:bg-white/[0.12] hover:text-white px-4 py-2 rounded-md'
-              }
-            >
-              {t('projects')}
-            </Tab>
-          </TabList>
-          <TabPanels className="mt-2">
-            <TabPanel className="bg-white p-4 rounded-md text-left">
-              <div className="flex justify-start space-x-4 mb-4">
-                <Link
-                  href={`/${lng}/projects/new`}
-                  className="bg-ming-blue hover:bg-deep-space text-white px-4 py-2 rounded-md"
-                >
-                  {t('createProject')}
-                </Link>
-                <Link
-                  href={`/${lng}/tokens`}
-                  className="bg-ming-blue hover:bg-deep-space text-white px-4 py-2 rounded-md"
-                >
-                  {t('tokens')}
-                </Link>
-                <button
-                  className="bg-ming-blue hover:bg-deep-space text-white px-4 py-2 rounded-md"
-                  onClick={openAddModal}
-                >
-                  {t('quickCreateCheck')}
-                </button>
-              </div>
-              <Projects lng={lng} />
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
 
-        <AddProjectModal
-          isOpen={showAddModal}
-          onClose={closeAddModal}
-          onAddProject={handleAddProject}
-          lng={lng}
-        />
-      </div>
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  return (
+    <div className="max-w-6xl mx-auto p-4">
+      <TabGroup>
+        <TabList className="bg-ming-blue flex p-2 w-full border border-th-secondary-300 rounded-xl shadow-md">
+          <Tab
+            className={({ selected }) =>
+              selected
+                ? 'bg-ming-blue text-white cursor-pointer text-2xl font-bold px-9 py-2 rounded-md'
+                : 'text-blue-100 hover:bg-white/[0.12] hover:text-white px-4 py-2 rounded-md'
+            }
+          >
+            {t('projects')}
+          </Tab>
+        </TabList>
+        <TabPanels className="mt-2">
+          <TabPanel className="bg-white p-4 rounded-md text-left">
+            <div className="hidden md:flex justify-start space-x-4 mb-4">
+              <Link
+                href={`/${lng}/projects/new`}
+                className="bg-ming-blue hover:bg-deep-space text-white px-4 py-2 rounded-md"
+              >
+                {t('createProject')}
+              </Link>
+              <Link
+                href={`/${lng}/tokens`}
+                className="bg-ming-blue hover:bg-deep-space text-white px-4 py-2 rounded-md"
+              >
+                {t('tokens')}
+              </Link>
+              <button
+                className="bg-ming-blue hover:bg-deep-space text-white px-4 py-2 rounded-md"
+                onClick={openAddModal}
+              >
+                {t('quickCreateCheck')}
+              </button>
+            </div>
+            <div className="md:hidden mb-4 flex justify-end" ref={menuRef}>
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <MenuButton
+                    className="bg-ming-blue hover:bg-deep-space text-white px-3 py-1 rounded-full border border-th-secondary-300"
+                    onClick={handleMenuToggle}
+                  >
+                    {t('...')}
+                  </MenuButton>
+                </div>
+                {isMenuOpen && (
+                  <MenuItems className="absolute right-0 mt-2 w-56 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+                    <div className="py-1">
+                      <MenuItem>
+                        {({ active }) => (
+                          <Link
+                            href={`/${lng}/projects/new`}
+                            className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}
+                          >
+                            {t('createProject')}
+                          </Link>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <Link
+                            href={`/${lng}/tokens`}
+                            className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm`}
+                          >
+                            {t('tokens')}
+                          </Link>
+                        )}
+                      </MenuItem>
+                      <MenuItem>
+                        {({ active }) => (
+                          <button
+                            onClick={openAddModal}
+                            className={`${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block w-full text-left px-4 py-2 text-sm`}
+                          >
+                            {t('quickCreateCheck')}
+                          </button>
+                        )}
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                )}
+              </Menu>
+            </div>
+            <Projects lng={lng} />
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+      <AddProjectModal
+        isOpen={showAddModal}
+        onClose={closeAddModal}
+        onAddProject={handleAddProject}
+        lng={lng}
+      />
     </div>
   )
 }
