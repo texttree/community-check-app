aa91e9a30e1e0976bc6d73eea4c0f55b9094962d
DenisArger <den.arger@gmail.com>
2024-07-05 15:29:30 +0300

    feat: edit AppBar

--- app/components/AppBar.js
+++ app/components/AppBar.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Bar from '@/public/bar.svg'
@@ -13,6 +13,8 @@ const AppBar = ({ lng }) => {
   const { t } = useTranslation(lng, 'common')
   const supabaseClient = createClient()
   const [user, setUser] = useState(null)
+  const router = useRouter()
+
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -22,13 +24,12 @@ const AppBar = ({ lng }) => {
           throw error
         }
       } catch (error) {
+        console.error('Error fetching user:', error)
         setUser(null)
       }
     }
     getUser()
   }, [supabaseClient.auth])
-
-  const router = useRouter()
 
   const handleLogout = async () => {
     try {
@@ -43,26 +44,63 @@ const AppBar = ({ lng }) => {
   return (
     <header className="bg-ming-blue p-4 flex justify-start items-center">
       <Menu as="div" className="relative inline-block text-left mr-4">
-        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          <Bar className="h-7 w-7" />
+        <MenuButton className="inline-flex items-center justify-center gap-x-1.5 rounded-md bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          <div className="flex items-center justify-center w-7 h-7">
+            <Bar />
+          </div>
         </MenuButton>
         <MenuItems className="absolute mt-2 w-56 p-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <MenuItem>
-            <SwitchLanguage lng={lng} className="hover:bg-blue-500" />
+          {user && (
+            <MenuItem as="div" className="flex items-center space-x-4 p-2">
+              <div>
+                <p className="text-gray-900 font-bold">{user.email}</p>
+              </div>
+            </MenuItem>
+          )}
+          <MenuItem as="div" className="my-2">
+            <SwitchLanguage lng={lng} />
+          </MenuItem>
+          <MenuItem as="div">
+            <div>
+              <div className="flex items-center py-1 text-center  mt-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5 stroke-th-text-primary mr-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5a17.92 17.92 0 0 1-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                  ></path>
+                </svg>
+                <p className="text-gray-700 mr-10 py-2 text-sm">{t('API')}</p>
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText('API_LINK_HERE')
+                    alert(t('apiLinkCopied'))
+                  }}
+                  className="bg-gray-200 rounded-md hover:bg-deep-space hover:text-white stext-gray-700 block ml-8 px-2 py-1 text-xs"
+                >
+                  {t('Копировать')}
+                </button>
+              </div>
+            </div>
           </MenuItem>
           {user ? (
-            <MenuItem>
+            <MenuItem as="div">
               <div
-                className="py-1 text-center px-2 mt-2 bg-gray-200 rounded-md hover:bg-red-500"
+                className="py-1 text-center px-2 mt-2 bg-gray-200 rounded-md hover:bg-red-500 cursor-pointer"
                 onClick={handleLogout}
               >
-                <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
-                  {t('signOut')}
-                </a>
+                <p className="text-gray-700 block px-4 py-2 text-sm">{t('signOut')}</p>
               </div>
             </MenuItem>
           ) : (
-            <MenuItem>
+            <MenuItem as="div">
               <Link
                 href={`/${lng}/login`}
                 className="text-gray-700 block px-4 py-2 text-sm text-center mt-2 bg-gray-200 rounded-md hover:bg-deep-space hover:text-white"
--- app/components/SwitchLanguage.js
+++ app/components/SwitchLanguage.js
@@ -1,32 +1,78 @@
 'use client'
 
+import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { languages } from '../i18n/settings'
 import Link from 'next/link'
-import { MenuItem } from '@headlessui/react'
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
 
 const SwitchLanguage = ({ lng }) => {
+  const [isOpen, setIsOpen] = useState(false)
   const pathName = usePathname()
+
   const redirectedPathName = (locale) => {
     if (!pathName) return '/'
     const segments = pathName.split('/')
     segments[1] = locale
     return segments.join('/')
   }
+
   return (
-    <div className="flex items-center justify-center space-x-2">
-      {languages.map((language) => (
-        <MenuItem key={language}>
-          <Link
-            href={redirectedPathName(language)}
-            className={`${
-              lng === language ? 'bg-ming-blue text-white' : 'bg-gray-200 text-gray-600'
-            } active:bg-deep-space active:text-white hover:bg-deep-space hover:text-white px-2 py-1 rounded-md`}
+    <div className="flex items-center justify-start space-x-2 mt-2">
+      <Menu as="div" className="relative inline-block text-left">
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            className="w-5 h-5 stroke-th-text-primary"
           >
-            {language}
-          </Link>
-        </MenuItem>
-      ))}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5a17.92 17.92 0 0 1-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+            ></path>
+          </svg>
+          <span className="ml-3 mr-12 text-sm text-gray-600">Language</span>{' '}
+          <MenuButton
+            className={`bg-gray-200 ml-8 mt-2${
+              isOpen ? 'bg-ming-blue text-white' : 'bg-gray-200 text-gray-600'
+            } active:bg-deep-space active:text-white hover:bg-deep-space hover:text-white px-2 py-1 rounded-md flex items-center`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {lng}
+          </MenuButton>
+        </div>
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <MenuItems className="absolute right-0 mt-2 text-sm bg-gray-200 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {languages.map((language) => (
+              <MenuItem key={language}>
+                {({ active }) => (
+                  <Link
+                    href={redirectedPathName(language)}
+                    className={`${
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                    } block px-4 py-2 hover:bg-gray-100 hover:text-gray-900 rounded-md`}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {language}
+                  </Link>
+                )}
+              </MenuItem>
+            ))}
+          </MenuItems>
+        </Transition>
+      </Menu>
     </div>
   )
 }
