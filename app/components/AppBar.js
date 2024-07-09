32c2fc150e0ca25015b29a52d69c6fa6b08ec275
DenisArger <den.arger@gmail.com>
2024-07-09 16:49:33 +0300

    style: the buttons are pressed to the right

--- app/components/AppBar.js
+++ app/components/AppBar.js
@@ -90,8 +90,8 @@ const AppBar = ({ lng }) => {
           {!isCheckPage && (
             <>
               <MenuItem as="div">
-                <div>
-                  <div className="flex items-center py-1 text-center  mt-2">
+                <div className="flex items-center justify-between py-1 text-center mt-2">
+                  <div className="flex items-center">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -106,17 +106,17 @@ const AppBar = ({ lng }) => {
                         d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5a17.92 17.92 0 0 1-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
                       ></path>
                     </svg>
-                    <p className="text-gray-700 mr-10 py-2 text-sm">{t('API')}</p>
-                    <button
-                      onClick={() => {
-                        navigator.clipboard.writeText(`${currentDomain}/doc`)
-                        toast.success(t('apiLinkCopied'))
-                      }}
-                      className="bg-gray-200 rounded-md hover:bg-deep-space hover:text-white stext-gray-700 block ml-8 px-2 py-1 text-xs"
-                    >
-                      {t('copy')}
-                    </button>
+                    <p className="text-gray-700 py-2 text-sm">{t('API')}</p>
                   </div>
+                  <button
+                    onClick={() => {
+                      navigator.clipboard.writeText(`${currentDomain}/doc`)
+                      toast.success(t('apiLinkCopied'))
+                    }}
+                    className="bg-gray-200 rounded-md hover:bg-deep-space hover:text-white text-gray-700 ml-4 px-2 py-1 text-xs"
+                  >
+                    {t('copy')}
+                  </button>
                 </div>
               </MenuItem>
               {user ? (
--- app/components/SwitchLanguage.js
+++ app/components/SwitchLanguage.js
@@ -21,26 +21,28 @@ const SwitchLanguage = ({ lng }) => {
   }
 
   return (
-    <div className="flex items-center justify-start space-x-2 mt-2">
+    <div className="flex items-center justify-between mt-2">
+      <div className="flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          viewBox="0 0 24 24"
+          className="w-5 h-5 stroke-th-text-primary"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5a17.92 17.92 0 0 1-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+          ></path>
+        </svg>
+        <span className="ml-3 text-sm text-gray-600">{t('language')}</span>
+      </div>
       <Menu as="div" className="relative inline-block text-left">
-        <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            viewBox="0 0 24 24"
-            className="w-5 h-5 stroke-th-text-primary"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5a17.92 17.92 0 0 1-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-            ></path>
-          </svg>
-          <span className="ml-3 mr-12 text-sm text-gray-600"> {t('language')}</span>{' '}
+        <div>
           <MenuButton
-            className={`bg-gray-200 ml-8 mt-2${
+            className={`bg-gray-200 ml-8 mt-2 ${
               isOpen ? 'bg-ming-blue text-white' : 'bg-gray-200 text-gray-600'
             } active:bg-deep-space active:text-white hover:bg-deep-space hover:text-white px-2 py-1 rounded-md flex items-center`}
             onClick={() => setIsOpen(!isOpen)}
@@ -60,17 +62,17 @@ const SwitchLanguage = ({ lng }) => {
           <MenuItems className="absolute right-0 mt-2 text-sm bg-gray-200 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             {languages.map((language) => (
               <MenuItem key={language}>
-                {({ active }) => (
-                  <Link
-                    href={redirectedPathName(language)}
-                    className={`${
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
-                    } block px-4 py-2 hover:bg-gray-100 hover:text-gray-900 rounded-md`}
-                    onClick={() => setIsOpen(false)}
-                  >
-                    {language}
-                  </Link>
-                )}
+                <Link
+                  href={redirectedPathName(language)}
+                  className={`${
+                    redirectedPathName(language) === pathName
+                      ? 'bg-gray-100 text-gray-900'
+                      : 'text-gray-700'
+                  } block px-4 py-2 hover:bg-gray-100 hover:text-gray-900 rounded-md`}
+                  onClick={() => setIsOpen(false)}
+                >
+                  {language}
+                </Link>
               </MenuItem>
             ))}
           </MenuItems>
