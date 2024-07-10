82fda5538482e2c289166a21a485c18dcf110706
DenisArger <den.arger@gmail.com>
2024-07-10 11:27:42 +0300

    style: resize pages

--- app/[lng]/client.js
+++ app/[lng]/client.js
@@ -18,86 +18,89 @@ const Index = ({ lng }) => {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="mx-auto p-4 text-center max-w-6xl">
+      <div className="mx-auto p-2 text-center max-w-4xl">
         <Image
           src="/logo.svg"
           alt="Community Check Logo"
-          className="mx-auto mb-6 -mt-16 md:-mt-32"
-          width={80}
-          height={80}
+          className="mx-auto mb-4 -mt-8 md:-mt-16"
+          width={60}
+          height={60}
         />
-        <h1 className="font-bold text-4xl md:text-8xl mb-4 text-ming-blue font-montserrat">
+        <h1 className="font-bold text-3xl md:text-6xl mb-2 text-ming-blue font-montserrat">
           Community Check
         </h1>
-        <p className="mx-auto max-w-2xl mb-10 text-sm md:text-lg text-center">
+        <p className="mx-auto max-w-xl mb-6 text-xs md:text-sm text-center">
           {t('aboutComCheck')}
         </p>
 
-        <div className="mb-10 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          <div className="w-full h-auto p-4 md:p-6 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
-            <div className="flex items-center mb-4">
+        <div className="mb-8 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          <div className="w-full h-auto p-2 md:p-4 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
+            <div className="flex items-center mb-2">
               <Image
                 src="/icon-check.svg"
                 alt="Check"
-                width={80}
-                height={80}
-                className="md:w-20 md:h-20"
+                width={60}
+                height={60}
+                className="md:w-16 md:h-16"
               />
             </div>
-            <h2 className="font-semibold text-lg md:text-xl">{t('checkText')}</h2>
-            <p className="flex-grow">{t('loginToAccess')}</p>
+            <h2 className="font-semibold text-base md:text-lg">{t('checkText')}</h2>
+            <p className="flex-grow text-xs md:text-sm">{t('loginToAccess')}</p>
           </div>
 
-          <div className="w-full h-auto p-4 md:p-6 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
-            <div className="flex items-center mb-4">
+          <div className="w-full h-auto p-2 md:p-4 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
+            <div className="flex items-center mb-2">
               <Image
                 src="/icon-comment.svg"
                 alt="Comment"
-                width={80}
-                height={80}
-                className="md:w-20 md:h-20"
+                width={60}
+                height={60}
+                className="md:w-16 md:h-16"
               />
             </div>
-            <h2 className="font-semibold text-lg md:text-xl">{t('comment')}</h2>
-            <p className="flex-grow">{t('loginToAccess')}</p>
+            <h2 className="font-semibold text-base md:text-lg">{t('comment')}</h2>
+            <p className="flex-grow text-xs md:text-sm">{t('loginToAccess')}</p>
           </div>
 
-          <div className="w-full h-auto p-4 md:p-6 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
-            <div className="flex items-center mb-4">
+          <div className="w-full h-auto p-2 md:p-4 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
+            <div className="flex items-center mb-2">
               <Image
                 src="/icon-project.svg"
                 alt="Project"
-                width={80}
-                height={80}
-                className="md:w-20 md:h-20"
+                width={60}
+                height={60}
+                className="md:w-16 md:h-16"
               />
             </div>
-            <h2 className="font-semibold text-lg md:text-xl">{t('curingProject')}</h2>
-            <p className="flex-grow">{t('loginToAccess')}</p>
+            <h2 className="font-semibold text-base md:text-lg">{t('curingProject')}</h2>
+            <p className="flex-grow text-xs md:text-sm">{t('loginToAccess')}</p>
           </div>
-          <div className="w-full h-auto p-4 md:p-6 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
-            <div className="flex items-center mb-4">
+          <div className="w-full h-auto p-2 md:p-4 text-left border border-gray-200 rounded-lg bg-white shadow flex flex-col">
+            <div className="flex items-center mb-2">
               <Image
                 src="/icon-project.svg"
                 alt="Project"
-                width={80}
-                height={80}
-                className="md:w-20 md:h-20"
+                width={60}
+                height={60}
+                className="md:w-16 md:h-16"
               />
             </div>
-            <h2 className="font-semibold text-lg md:text-xl">
+            <h2 className="font-semibold text-base md:text-lg">
               {t('integrationProjects')}
             </h2>
-            <Link className="flex-grow" href="/doc">{`${currentDomain}/doc`}</Link>
+            <Link
+              className="flex-grow text-xs md:text-sm"
+              href="/doc"
+            >{`${currentDomain}/doc`}</Link>
           </div>
         </div>
-        <div className="mx-auto my-4 h-auto w-full md:w-2/5 flex  flex-row justify-between items-center bg-ming-blue rounded-lg px-6 py-4">
-          <p className="text-white text-center text-sm md:text-base">
+        <div className="mx-auto my-4 h-auto w-full md:w-3/5 flex flex-row justify-between items-center bg-ming-blue rounded-lg px-4 py-2">
+          <p className="text-white text-center text-xs md:text-sm">
             {t('startCheсking')}
           </p>
           <Link
             href="/login"
-            className="px-6 py-2 h-10 text-ming-blue rounded bg-white hover:bg-gray-200 text-center flex items-center"
+            className="px-4 py-1 h-8 text-ming-blue rounded bg-white hover:bg-gray-200 text-center flex items-center text-xs md:text-sm"
           >
             {t('signIn')}
           </Link>
