56991b01027f2ad3f22b4d7bf7b4f9d3a9011f7b
DenisArger <den.arger@gmail.com>
2024-07-10 10:14:36 +0300

    fix: fix for merge

--- app/[lng]/projects/client.js
+++ app/[lng]/projects/client.js
@@ -19,8 +19,15 @@ const ProjectPage = ({ lng }) => {
   const [projects, setProjects] = useState([])
   const menuRef = useRef(null)
   const router = useRouter()
+  const { data: projectsData, mutate, error } = useSWR('/api/projects', fetcher)
 
-  const openAddModal = () => {
+  useEffect(() => {
+    if (projectsData) {
+      setProjects(projectsData)
+    }
+  }, [projectsData])
+
+  const openAddModal = (windowTitle, showBook, showCheck) => {
     setShowAddModal(true)
     document.body.style.overflow = 'hidden'
     setModalOptions({ windowTitle, showBook, showCheck })
@@ -35,9 +42,9 @@ const ProjectPage = ({ lng }) => {
   const handleAddFastCheck = async (data) => {
     try {
       const response = await axios.post('/api/projects/complex-create', {
-        project_name: projectName,
-        book_name: book,
-        check_name: check,
+        project_name: data.projectName,
+        book_name: data.book,
+        check_name: data.check,
       })
       if (response.status === 201) {
         router.push(
@@ -103,7 +110,7 @@ const ProjectPage = ({ lng }) => {
   })
 
   return (
-    <div className="max-w-6xl mx-auto p-4 relative">
+    <div className="max-w-6xl mx-auto p-4">
       <TabGroup>
         <TabList className="bg-ming-blue flex p-2 w-full border border-th-secondary-300 rounded-t-xl shadow-md">
           <Tab
@@ -192,9 +199,9 @@ const ProjectPage = ({ lng }) => {
               errorMessage={errorMessage}
               lng={lng}
               showProject={true}
-              showBook={true}
-              showCheck={true}
-              windowTitle="quickCreateCheck"
+              showBook={modalOptions.showBook}
+              showCheck={modalOptions.showCheck}
+              windowTitle={modalOptions.windowTitle}
             />
           </div>
         </>
